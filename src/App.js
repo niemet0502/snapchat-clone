d569d7fa408928fdec6ec53b35635ecc254e00dd
niemet0502 <vincentmarius8@gmail.com>
2021-01-15 00:49:50 +0100

    main :: used webcamCapture function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import WebcamCapture from "./WebcamCapture"
 function App() {
   return (
     <div className="App">
-
+      <WebcamCapture />
     </div>
   );
 }
