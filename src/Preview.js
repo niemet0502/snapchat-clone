af833557d8ffb93293869d6066a3db3107ddae90
niemet0502 <vincentmarius8@gmail.com>
2021-01-26 21:32:59 +0100

    main :: import screenshoot from store

--- src/Preview.js
+++ src/Preview.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useSelector } from "react-redux"
+import { selectCameraImage } from "./features/cameraSlice"
 
 function Preview() {
+  const cameraImage = useSelector()
   return (
-    <div>
-      
+    <div className="preview">
+      <img src="" alt=""/>
     </div>
   )
 }
